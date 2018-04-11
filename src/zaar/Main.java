5a57e2a7ba679b2b7cff43dfe243eb6a4f2cab76
Emil Assarsson <emilassarsson@Emils-MBP.lan>
2018-04-11 21:58:17 +0200

    Test Emil

--- src/zaar/Main.java
+++ src/zaar/Main.java
@@ -3,6 +3,7 @@ package zaar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        TestClass1 test1 = new TestClass1();
+        test1.callMess();
     }
 }
