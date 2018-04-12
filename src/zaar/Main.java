3b63d6accb8d94d053320c8fc6548b0e8717419d
DA120B-CELD <christian.zaar0004@stud.hkr.se>
2018-04-12 09:07:58 +0200

    Test

--- src/zaar/Main.java
+++ src/zaar/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         TestClass1 test1 = new TestClass1();
         test1.callMess();
+        System.out.println("hello");
     }
 }
