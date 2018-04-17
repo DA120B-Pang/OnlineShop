a6771356c09a35f9329d47e45273448840a33dcb
martinrydstrom <rydstrom.martin@gmail.com>
2018-04-17 09:55:36 +0200

    Test Martin

--- src/zaar/TestClass1.java
+++ src/zaar/TestClass1.java
@@ -2,9 +2,10 @@ package zaar;
 
 public class TestClass1 {
     private String mess = "Emil";
-    String test = "w"
+    String test = "w";
 
     public void callMess(){
         System.out.println(mess);
+        System.out.println(test);
     }
 }
