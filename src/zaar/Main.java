a6e35ffe26de9fd262940022ed537582a2972d2e
kubbkungen <emil.assarsson@hotmail.se>
2018-04-18 12:23:54 +0200

    test

--- src/zaar/Main.java
+++ src/zaar/Main.java
@@ -3,8 +3,6 @@ package zaar;
 public class Main {
 
     public static void main(String[] args) {
-        TestClass1 test1 = new TestClass1();
-        test1.callMess();
-        System.out.println("hello");
+
     }
 }
