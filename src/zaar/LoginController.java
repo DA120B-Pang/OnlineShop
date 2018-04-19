ed9c6d2188629f77a2df6daf79d6c787f12ece2f
kubbkungen <emil.assarsson@hotmail.se>
2018-04-19 10:19:36 +0200

    två scener

---
+++ src/zaar/LoginController.java
@@ -0,0 +1,35 @@
+package zaar;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @FXML private Button backButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void useBackButton(ActionEvent e) throws IOException {
+        Node node = (Node)e.getSource();
+        Stage stage = (Stage)node.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sampleProduct.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+}
---
+++ src/zaar/ProductController.java
@@ -0,0 +1,41 @@
+package zaar;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProductController implements Initializable {
+
+    @FXML private Button loginButton;
+    @FXML private Button kundVButton;
+    @FXML private Button searchButton;
+    @FXML private TextField searchField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void useLoginButton(ActionEvent e) throws IOException {
+        Node node = (Node)e.getSource();
+        Stage stage = (Stage)node.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sampleLogin.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+}
