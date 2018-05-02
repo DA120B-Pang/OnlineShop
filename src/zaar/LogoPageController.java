aade331e694bfb13ae8d1a94e6b5021ee78fdbad
ville <villetorstensson@hotmail.com>
2018-05-02 13:39:19 +0200

    Uppdaterat första-sidan

--- src/zaar/LogoPageController.java
+++ src/zaar/LogoPageController.java
@@ -1,5 +1,8 @@
 package zaar;
 
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,11 +11,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,7 +27,7 @@ public class LogoPageController implements Initializable {
     @FXML
     private ImageView logo;
     @FXML
-    private Button moveOn;
+    private Label moveOn;
     @FXML
     private AnchorPane pane;
 
@@ -30,19 +36,28 @@ public class LogoPageController implements Initializable {
         Image image = new Image("File:img/ShopIT.png");
         pane.getChildren().add(new ImageView(image));
 
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1.2), moveOn);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.0);
+        fadeTransition.setCycleCount(Animation.INDEFINITE);
+        fadeTransition.play();
 
 
     }
 
-    public void useMoveOnButton(ActionEvent e) throws IOException {
-        Node node = (Node) e.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+    public void useMoveOnButton(javafx.scene.input.MouseEvent e) throws IOException {
+        try {
+            Node node = (Node) e.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("product/Product.fxml"));
-        Parent root = loader.load();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("product/Product.fxml"));
+            Parent root = loader.load();
 
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (Exception ex) {
+            System.out.println("Switched scene");
+        }
 
+        }
     }
-}
